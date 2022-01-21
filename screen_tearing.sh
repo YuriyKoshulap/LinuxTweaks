62dadb9056413b0bf255ea835c9120ee621079c3
YuriyKoshulap <urkosh@outlook.com>
2022-01-21 19:13:12 +0100

    removed #!/bin/bash and warranty disclaimer

--- screen_tearing.sh
+++ screen_tearing.sh
@@ -1,24 +1,3 @@
-#!/bin/bash
-# This is an example of a File Header comments in Bash
-#
-# Copyright 2022 Yuriy Koshulap
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-#######################################
-#
 # On some Linux desktops environments with non OpenGL (e.g. xRandR)
 # this code prevents screen tearing
 # on my old Dell Inspiron 14R (N4010) with Intel Core HD Graphics (915)
@@ -29,7 +8,6 @@
 #
 # Hence, normally I create it with "sudo nano /usr/share/X11/xorg.conf.d/20-intel.conf"
 # and copy/paste the code below 
-########################################
 
 Section "Device"
   Identifier  "Intel Graphics"
