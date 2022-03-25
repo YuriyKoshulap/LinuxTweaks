fc42e9994a06e04bbcba1150804a672680822f96
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2022-03-25 21:15:35 +0100

    Create Screen Color Temperature.sh

---
+++ Screen Color Temperature.sh
@@ -0,0 +1,6 @@
+# On Debian/Ubuntu distributions with the desktop environments other than Gnome or KDE Plasma install:
+sudo apt install sct
+# Set the temperature with:
+sct [temperature]
+# where temperature is a nmber between 1000 and 10000
+# add sct [4000] to autostart
