36af494081a2be40b72ef25794f161d7186a0896
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2022-03-25 21:35:37 +0100

    Update wifi_issue.sh

--- wifi_issue.sh
+++ wifi_issue.sh
@@ -1,5 +1,8 @@
 # If Wifi suddenly drops disabling 802.11n standard improves
 # this solution solved issue on "Intel Corporation Centrino Wireless-N 1000"
 
-# Solution: add the line below to /etc/modprobe.d/iwlwifi.conf
+# Solution:
+sudo nano /etc/modprobe.d/iwlwifi.conf
+# add the following line:
 options iwlwifi 11n_disable=1 led_mode=1 swcrypto=1
+# Save (Ctrl+o) and exit (Ctrl+x)
