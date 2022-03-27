9059e48556fcb3622253369a890bd9c2aee7e470
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2022-03-27 09:49:33 +0200

    Update backlight.sh

--- backlight.sh
+++ backlight.sh
@@ -2,17 +2,20 @@
 # this solution worked for Dell Inspiron 4010 with
 # Intel Core HD Graphics card
 #
-# to the grub (sudo nano /etc/default/grub)
-# section "GRUB_CMDLINE_LINUX_DEFAULT=" add:
 #
+# 1. edit the grub "GRUB_CMDLINE_LINUX_DEFAULT=" line
+sudo nano /etc/default/grub
+# by adding:
 acpi_backlight=none
+# (Ctrl-o to save, Crtl-x to exit nano)
 #
-# then update grub
-# Debian/buntu:
+#
+# 2. then update grub
+# on Debian/buntu:
 sudo update-grub
-# Arch/Manjaro
+# on Arch/Manjaro
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-# OpenSuse
+# on OpenSuse
 -su
 grub2-mkconfig
 # or use Yast Boot Loader (GUI)
