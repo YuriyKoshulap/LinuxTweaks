2e6ffd94b65ab05b35316b794ce73185d77b63dd
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2022-04-24 19:06:57 +0200

    Create One-Liners.sh

---
+++ One-Liners.sh
@@ -0,0 +1,17 @@
+# Run the last command as root
+sudo !!
+
+# Restore a broken terminal
+reset
+
+# My IP address
+curl ifconfig.me
+
+# See the ASCII table
+man ascii
+
+# Network activity in realtime
+lsof -i
+
+# remind to leave in 15 min
+leave +15
