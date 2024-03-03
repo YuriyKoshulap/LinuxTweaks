903ed63d9a3be26c74eab1bc189669b20df6808f
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2024-03-03 18:49:20 -0500

    Update OpenSuse.sh
    update 1

--- OpenSuse.sh
+++ OpenSuse.sh
@@ -2,4 +2,12 @@
 
 https://www.reddit.com/r/openSUSE/comments/i57ld8/what_is_zypper_equivalent_of_apt_autoremove/
 
+# dependencies/requires: awk, grep and xargs
+
+#~~~~~~~~~~~~~~~~~~~~~~
+
+#a more simple solution:
+
+clean() { zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs -r zypper remove --clean-deps; }
+
 #source: https://www.reddit.com/r/openSUSE/comments/i57ld8/what_is_zypper_equivalent_of_apt_autoremove/
