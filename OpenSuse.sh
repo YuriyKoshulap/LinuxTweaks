f26613f8da627bd1fdbeed6a9c563ebe696dad02
Yura <94837852+YuriyKoshulap@users.noreply.github.com>
2024-03-03 18:45:21 -0500

    Create OpenSuse.sh
    the first OpenSuse-related tweak

---
+++ OpenSuse.sh
@@ -0,0 +1,5 @@
+#What is zypper equivalent of 'apt autoremove'?
+
+https://www.reddit.com/r/openSUSE/comments/i57ld8/what_is_zypper_equivalent_of_apt_autoremove/
+
+#source: https://www.reddit.com/r/openSUSE/comments/i57ld8/what_is_zypper_equivalent_of_apt_autoremove/
